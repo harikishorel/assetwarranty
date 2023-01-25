7673eb3693bcdfee021aa76d39f3f4acb7b7378d
HARIKISHORE <harikishore1258@gmail.com>
2023-01-25 13:02:44 +0530

    hk made changes

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,34 @@
 <template>
-
+  <div>
+    <v-form>
+      <v-file-input
+        v-model="pdf"
+        ref="pdfInput"
+        label="Upload PDF"
+        @change="uploadPdf"
+      ></v-file-input>
+    </v-form>
+    <a v-if="pdfUrl" :href="pdfUrl" target="_blank">Open PDF</a>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'IndexPage'
-}
+  data() {
+    return {
+      pdf: null,
+      pdfUrl: "",
+    };
+  },
+  methods: {
+    uploadPdf() {
+      if (!this.pdf) return;
+      let reader = new FileReader();
+      reader.readAsDataURL(this.pdf);
+      reader.onload = (e) => {
+        this.pdfUrl = e.target.result;
+      };
+    },
+  },
+};
 </script>
